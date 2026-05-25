24996d452e542172bbbb3b65e2b90968219a775e
TheodorAlexandru <theodor-alexandru.talmaciu@student.tuiasi.ro>
2026-05-25 17:13:27 +0300

    adaugare comentarii copyright

--- ChestionareAuto/Logic/Quiz.cs
+++ ChestionareAuto/Logic/Quiz.cs
@@ -1,13 +1,13 @@
 ﻿/**************************************************************************
  * *
- * File:        Quiz.cs                                              *
+ * File:        Quiz.cs                                                  *
  * Copyright:   (c) 2026, Luca Monica, Macovei Paul, Talmaciu Theodor    *              
- * Description: Această clasă reprezintă modelul de date pentru o        *
- *              întrebare, conținând textul, variantele și imaginile.    *
- * Author:      Luca Monica, Macovei Paul, Talmaciu Theodor              *
+ * Description: Această clasă reprezintă un chestionar activ,            *
+ *              gestionând starea curentă a sesiunii.                    *
+ * Author:      Luca Monica                                              *
  * Proiect:     Chestionare Auto                                         *
                                          
- * *
+ *                                                                       *
  * Acest software a fost dezvoltat de 3 studenți ca proiect educațional  *
  * și a fost conceput pentru a fi utilizat în mod gratuit de către       *
  * oricine dorește să învețe sau să se testeze pentru examenul auto.     *
--- ChestionareAuto/Logic/QuizManager.cs
+++ ChestionareAuto/Logic/QuizManager.cs
@@ -1,13 +1,13 @@
 ﻿/**************************************************************************
  * *
- * File:        Question.cs                                              *
+ * File:        QuizManager.cs                                           *
  * Copyright:   (c) 2026, Luca Monica, Macovei Paul, Talmaciu Theodor    *              
- * Description: Această clasă reprezintă modelul de date pentru o        *
- *              întrebare, conținând textul, variantele și imaginile.    *
- * Author:      Luca Monica, Macovei Paul, Talmaciu Theodor              *
+ * Description: Această clasă implementează șablonul de proiectare       *
+ *              Singleton pentru gestionarea stării globale a aplicației.*
+ * Author:      Luca Monica                                              *
  * Proiect:     Chestionare Auto                                         *
                                          
- * *
+ *                                                                       *
  * Acest software a fost dezvoltat de 3 studenți ca proiect educațional  *
  * și a fost conceput pentru a fi utilizat în mod gratuit de către       *
  * oricine dorește să învețe sau să se testeze pentru examenul auto.     *
